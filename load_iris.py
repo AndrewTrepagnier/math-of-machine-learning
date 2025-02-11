feature scaling and fixed bugs

load_iris.py
@@ -21,8 +21,10 @@ y = np.where(y == 'IRIS-Setosa', -1, 1)
 
 X = df.iloc[0:100, [0,2]].values
 
-# plt.scatter(X[:50, 0], X[:50, 1], color='blue', marker='x', label='setosa')
-# plt.scatter(X[50:100, 0], X[50:100, 1], color='red', marker='o', label='versicolor')
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.title("Iris Feature Dataset")
 
-
-# plt.show()
+plt.show()